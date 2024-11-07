ef4426c49918a49f84749a68078f1b163b3ffa81
naren4545 <priyanshusingh.connect@gmail.com>
2024-11-07 12:59:13 +0530

    Your commit message

--- src/app/components/home/ui/HeroSlider.tsx
+++ src/app/components/home/ui/HeroSlider.tsx
@@ -1,15 +1,14 @@
-
-
 import React from 'react';
-import img1 from '../../../assests/Features.svg'
-import img2 from '../../../assests/Events.svg'
-import img3 from '../../../assests/Offers.svg'
-import img4 from '../../../assests/Collaborations.svg'
-import img5 from '../../../assests/Promotions.svg'
-import img6 from '../../../assests/Announcements.svg'
-import img7 from '../../../assests/Services.svg'
+import img1 from '../../../assests/Features.svg';
+import img2 from '../../../assests/Events.svg';
+import img3 from '../../../assests/Offers.svg';
+import img4 from '../../../assests/Collaborations.svg';
+import img5 from '../../../assests/Promotions.svg';
+import img6 from '../../../assests/Announcements.svg';
+import img7 from '../../../assests/Services.svg';
 import Image from 'next/image';
-import style  from './HeroSlider.module.css';
+import style from './HeroSlider.module.css';
+
 const HeroSlider: React.FC = () => {
   const images = [
     img1,
@@ -23,14 +22,13 @@ const HeroSlider: React.FC = () => {
 
   return (
     <div className="relative overflow-hidden w-full max-w-[1200px] mx-auto">
+      {/* Left and right blurry edges */}
      
       <div className={`flex w-[calc(300px*14)] items-end ${style.animatemarquee} pt-10`}>
-     
-
         {/* Double the array to create a seamless loop */}
-        {[...images, ...images,...images].map((image, index) => (
+        {[...images, ...images, ...images].map((image, index) => (
           <div key={index} className="flex-shrink-0 w-auto h-[147px] p-4">
-            <Image src={image} alt={`Image ${index + 1}`} className="w-full  h-full object-cover" />
+            <Image src={image} alt={`Image ${index + 1}`} className="w-full h-full object-cover" />
           </div>
         ))}
       </div>
