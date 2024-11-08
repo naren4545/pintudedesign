b8a6f79b969d50676dcc268aa7b7944cfd1bea7d
naren4545 <priyanshusingh.connect@gmail.com>
2024-11-08 10:03:00 +0530

    changes

--- src/app/components/home/Section1.tsx
+++ src/app/components/home/Section1.tsx
@@ -1,19 +1,21 @@
 import React from "react";
-import img from '../../assests/aboutPromo.svg'
+import img from '../../assests/rafiki.png'
 import Image from "next/image";
 import style from './CustomBullet.module.css'
 export default function Section1() {
   return (
     <section id="about">
-      <div className="max-w-[1327px] mx-auto p-4 py-10 ">
+      <div className=" py-10 ">
 <h2 className="font-bold text-5xl center border-b-[4px] w-fit mx-auto  border-[#F6830E]">About Pintude</h2>
       
       <p className="text-center text-2xl font-light max-w-[837px] pt-5 mx-auto font-m">Pintude empowers local businesses to effortlessly build an online presence and connect 
 with nearby customers. Our easy-to-use platform helps showcase products and 
 services without technical hassle or high costs.</p>
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
-  <div className=" p-4 flex items-center"><Image src={img} alt="about"/></div>
-  <div className=" col-span-1 lg:col-span-2 p-4">
+      <div className="  aboutImg py-10">
+
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 max-w-[1327px] mx-auto ">
+  <div className=" p-4 flex items-center"><Image src={img} alt="about" className="invisible"/></div>
+  <div className=" col-span-1 lg:col-span-2 p-4 max-w-[759px] lg:ml-auto">
     <h3 className=" font-semibold text-[#E78B01] font-r text-5xl py-5">Our Mission</h3>
   
     <div className={`${style.custombullet} font-r`}>
@@ -29,13 +31,13 @@ large budgets.</p>
 
 
   </div>
-  
+  </div>
 </div>
 
 
-<div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+<div className="grid grid-cols-1 lg:grid-cols-3 pt-10 gap-4">
   
-  <div className=" col-span-1 lg:col-span-2 p-4">
+  <div className=" col-span-1 lg:col-span-2 p-4 py-10 aboutConbg">
     
 
 <h2 className="font-semibold text-[#E78B01] font-r text-5xl py-5">Our Core Values:</h2>
